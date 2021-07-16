420ce317630a5dfd76845c87045157572e52f3d4
イ ジニョン <lee@aoi.aoimac.co.jp>
2021-07-16 15:50:29 +0900

    카카오 모듈 완성

--- static/exocmdprcsr.js
+++ static/exocmdprcsr.js
@@ -1,6 +1,28 @@
-<script>
-//외부모듈 테스트
-    function test2() {
-        alert("test2 실행됨")
+function getmymsg(cmd, param) {
+    var arrret = [];
+    var arrpres = [];
+    var rawhtml = "";
+    try {
+        rawhtml = org.jsoup.Jsoup.connect("https://exoman.herokuapp.com/kakaocmd" + cmd + "/" + param.replace(" ", "%20")).get();
+        arrpres = html.select("pre");
+        arrret = arrpres
+
+    } catch (e) {
+        arrret.push("서버와 연결할 수 없읍니다.");
+        nally {
+            return arrret;
+        }
     }
-</script>
+};
+
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    var cmd = msg.split(" ")[0];
+    var param = msg.replace(cmd + " ", "");
+    var mymsg = "";
+    if (cmd.startsWith("/")) {
+        mymsg = getmymsg(cmd, param);
+        if (mymsg.length() > 0) {
+            mymsg.forEach(e => replier.reply(e));
+        }
+}
