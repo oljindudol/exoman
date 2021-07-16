0cbcdc56c567b3842f3c17dd352246b7960b6b3e
イ ジニョン <lee@aoi.aoimac.co.jp>
2021-07-16 11:15:35 +0900

    js파일 수정

--- exocmdprcsr.js
+++ exocmdprcsr.js
@@ -1,6 +1,7 @@
+<script language="javascript" type="text/javascript" >
 
 //외부모듈 테스트
-function test2() {
-    alert("test2 실행됨");
-}
-
+    function test2() {
+        alert("test2 실행됨")
+    }
+</script>
