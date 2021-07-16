a8db078afa1820cde5ee1009bdcc4c9dc8751866
YOOOjung <oljindudol@naver.com>
2021-07-16 22:23:15 +0900

    버그수정등

--- static/exocmdprcsr.js
+++ static/exocmdprcsr.js
@@ -1,33 +1,32 @@
 function getmymsg(cmd, param) {
-    var arrret = [];
-    var arrpres = [];
-    var rawhtml = "";
-    var eurl = "";
-    try {
-      eurl = "https://exoman.herokuapp.com/kakaocmd" + cmd + "/" + param.replace(" ", "%20");
-      rawhtml = org.jsoup.Jsoup.connect(eurl).get();
-      arrpres = rawhtml.select("pre");
-      arrret.push(arrpres);
-    }  catch (e) {
-    arrret.push("서버와 연결할 수 없읍니다.");
-    arrret.push(e);
+  var arrret = [];
+  var arrpres = [];
+  var rawhtml = "";
+  var eurl = "";
+  try {
+    eurl = "https://exoman.herokuapp.com/kakaocmd" + cmd + "/" + param.replace(" ", "%20");
+    rawhtml = org.jsoup.Jsoup.connect(eurl).get();
+    arrpres = rawhtml.select("pre");
+    arrret.push(arrpres);
+  }  catch (e) {
+  arrret.push("서버와 연결할 수 없읍니다.");
+  arrret.push(e);
+}
+ finally   {
+    return arrret;
   }
-   finally   {
-      return arrret;
+}
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  var cmd = msg.split(" ")[0];
+  var param = msg.toString().replace(cmd, "").trim();
+  var mymsg=[];
+  if (param === undefined) {
+    param = "none"
+    ;}
+  if (cmd.startsWith("/")) {
+    mymsg = getmymsg(cmd, param);
+    if (Object.keys(mymsg).length> 0) {
+      mymsg.forEach(e => replier.reply(e.toString().replace(/<[^>]+>/g,"").trim()));
     }
   }
-  function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    var cmd = msg.split(" ")[0];
-    var param = msg.toString().replace(cmd, "").trim();
-    var mymsg=[];
-    if (param === undefined) {
-      param = "none"
-      ;}
-    if (cmd.startsWith("/")) {
-      mymsg = getmymsg(cmd, param);
-      if (Object.keys(mymsg).length> 0) {
-        mymsg.forEach(e => replier.reply(e.toString().replace(/<[^>]+>/g,"").trim()));
-      }
-    }
-  }
-  
+}
